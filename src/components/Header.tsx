c81d32c3fc5a5e4c3c2185d45eaf5bf98b40a78c
Bruno Braga <brunomiguel1997dh@gmail.com>
2026-09-15 23:29:36 +0200

    feat(header): ajout du composant Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  const titleHeader = "Mon budget";
+  
+  return (
+    <header className="header">
+      <h1 className="header__titre">{titleHeader}</h1>
+    </header>
+  );
+}
+
+export default Header;
